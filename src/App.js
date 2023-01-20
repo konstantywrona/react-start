bf14238ffb10b547c6b92f089132d39da59dbca6
Konstanty Wrona <koswro@gmail.com>
2023-01-20 19:25:18 +0100

    Excercise 12.1-12.6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Hero from './components/Hero/Hero';
 import List from './components/List/List';
 import SearchForm from './components/SearchForm/SearchForm';
+import Container from './components/Container/Container';
 
 const App = () => {
   return (
-    <div>
+    <Container>
       <Hero />
       <SearchForm />
       <List />
-    </div>
+    </Container>
   );
 };
 
